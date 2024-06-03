e4e446a50ab5ad9c6df16d246ea1b80eb2679bda
tosynthegeek <121390044+tosynthegeek@users.noreply.github.com>
2024-06-03 01:18:02 +0100

    Update run-supra.js

--- scripts/run-supra.js
+++ scripts/run-supra.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const address = "testnet-dora.supraoracles.com";
 const pairIndexes = [0, 21, 61, 49];
-const sepoliaPullContractAdress = ""; //Update for V1 or V2
+const sepoliaPullContractAdress = "0x6Cd59830AAD978446e6cc7f6cc173aF7656Fb917"; //Update for V1 or V2
 const privateKey = process.env.PRIVATE_KEY;
 
 async function main() {
@@ -13,6 +13,8 @@ async function main() {
     pair_indexes: pairIndexes,
     chain_type: "evm",
   };
+
+  console.log("Getting proof....");
 
   const proof = client.getProof(request, (err, response) => {
     if (err) {
@@ -40,22 +42,21 @@ async function callContract(response) {
   console.log("Estimated gas for deliverPriceData:", gasEstimate.toString());
   console.log("Estimated gas price:", gasPrice.toString());
 
-  const transactionObject = {
+  const tx = {
     from: "0xDA01D79Ca36b493C7906F3C032D2365Fb3470aEC",
     to: contractAddress,
     data: txData,
     gas: gasEstimate,
-    gasPrice: gasPrice, // Set your desired gas price here, e.g: web3.utils.toWei('1000', 'gwei')
+    gasPrice: gasPrice,
   };
 
   const wallet = new ethers.Wallet(privateKey);
-  const signedTransaction = await wallet.signTransaction(transactionObject);
-  const provider = await ethers.provider; // Send the signed transaction and await confirmation
-  const txHash = await provider.sendTransaction(signedTransaction);
-  console.log("Transaction sent! Hash:", txHash);
+  const signedTransaction = await wallet.signTransaction(tx);
+  const txResponse = await wallet.sendTransaction(tx);
+  console.log("Transaction sent! Hash:", txResponse.hash);
 
   // (Optional) Wait for transaction confirmation (e.g., 1 block confirmation)
-  const receipt = await provider.waitForTransaction(txHash, 1);
+  const receipt = await txResponse.wait(1);
   console.log("Transaction receipt:", receipt);
 }
 
